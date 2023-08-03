e9b102d85ee84778c553b118d0baa80745c6252e
Sam <sammeown@gmail.com>
2023-08-03 19:38:28 +0300

    Migrate Mesto to React - added api, used api to load user info

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,35 @@
+import { useEffect, useState } from 'react';
 import avatar_placeholder from '../images/avatar_placeholder.png'
+import api from "../utils/api";
 
 
 function Main({onEditProfile, onEditAvatar, onAddPlace}) {
+  const [userName, setUserName] = useState(null);
+  const [userDescription, setUserDescription] = useState(null);
+  const [userAvatar, setUserAvatar] = useState(null);
+
+  useEffect(() => {
+    api.getUserInfo()
+      .then(data => {
+        const user = data;
+        setUserName(user.name);
+        setUserDescription(user.about);
+        setUserAvatar(user.avatar);
+      });
+  });
+
   return (
     <main className="content">
       <section className="profile page__profile">
         <span className="profile__picture-container" onClick={onEditAvatar}>
-          <img src={avatar_placeholder} alt="Аватар" className="profile__picture" />
+          <img src={userAvatar ?? avatar_placeholder} alt="Аватар" className="profile__picture" />
         </span>
         <div className="profile__info">
           <div className="profile__name-container">
-            <h1 className="profile__name overflow-ready-string"></h1>
+            <h1 className="profile__name overflow-ready-string">{userName ?? ""}</h1>
             <button className="btn profile__edit-btn" type="button" onClick={onEditProfile} aria-label="Редактировать профиль"></button>
           </div>
-          <p className="profile__about overflow-ready-string"></p>
+          <p className="profile__about overflow-ready-string">{userDescription ?? ""}</p>
         </div>
         <button className="btn profile__add-btn" type="button" onClick={onAddPlace} aria-label="Добавить место"></button>
       </section>
