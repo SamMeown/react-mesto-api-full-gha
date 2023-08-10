d442efbfd3bf2c31de12bbaf91a3cf0de16bd5e2
Sam <sammeown@gmail.com>
2023-08-10 11:30:17 +0300

    Auth for Mesto - added navigation to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,12 @@ function Header() {
   return (
     <header className="header page__header">
       <img src={logo} alt="Лого" className="logo" />
+      <nav className="header__menu">
+        <p className="header__menu-info header__menu_visible">email@example.com</p>
+        <a className="header__menu-link header__menu_visible" href="#">Регистрация</a>
+        <a className="header__menu-link header__menu_visible" href="#">Войти</a>
+        <a className="header__menu-link" href="#">Выйти</a>
+      </nav>
     </header>
   );
 }
