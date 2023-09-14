b1d9a8409f6d61a810a4275c77eefcfa9bed5d52
Sam <sammeown@gmail.com>
2023-09-14 14:30:55 +0300

    reg and auth: auth middleware - don't send error from the handler

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken');
 const authErrors = require('../errors/auth');
+const httpErrors = require('../errors/http');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: authErrors.needsAuth });
+    return next(new httpErrors.UnauthorizedError(authErrors.needsAuth));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -14,7 +15,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'dev-secret-key');
   } catch (err) {
-    return res.status(401).send({ message: authErrors.needsAuth });
+    return next(new httpErrors.UnauthorizedError(authErrors.needsAuth));
   }
 
   req.user = payload;
